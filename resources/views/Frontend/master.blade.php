053a9b3a5179ce99a13294a8839ba8df4331cf73
Michael <mickeymurage@gmail.com>
2024-04-11 09:04:33 +0300

    comment out bootstrap imports

--- resources/views/Frontend/master.blade.php
+++ resources/views/Frontend/master.blade.php
@@ -11,8 +11,8 @@
     <meta name="keywords" content="">
     <title>HR HUB 360</title>
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
     <link href="https://easetemplate.com/free-website-templates/hrms/css/bootstrap.min.css" rel="stylesheet">
     <!-- Style CSS -->
     <link href="https://easetemplate.com/free-website-templates/hrms/css/style.css" rel="stylesheet">
@@ -146,9 +146,9 @@
             });
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+    </script> -->
     @notifyJs
     <script src="https://kit.fontawesome.com/5c95e5cc68.js" crossorigin="anonymous"></script>
 
