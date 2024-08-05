28af44f0cd9233a350e591999060194f806bae74
Michael-Murage <mickeymurage@gmail.com>
2024-08-05 10:41:59 +0300

    added work number to the regulatr tasks section

--- resources/js/Pages/Tasks.jsx
+++ resources/js/Pages/Tasks.jsx
@@ -120,11 +120,14 @@ function Tasks() {
           <TaskStatusColorCode />
           <ApartmentCodes />
           <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <TableComp columns={['Name', 'Description', 'From Date', 'To Date', 'Status', 'Finished At', 'Feedback', 'Action']}>
+            <TableComp columns={['No', 'Name', 'Description', 'From Date', 'To Date', 'Status', 'Finished At', 'Feedback', 'Action']}>
               {
                 (Array.isArray(tasks.data) ? tasks.data : []).map((task, index) => {
                   return (
                     <tr key={task.id || index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <th scope="row" className="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        { `#${task.id}` }
+                      </th>
                       <th scope="row" className="px-2 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         { task.name }
                       </th>
