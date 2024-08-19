a121524784c0c868350ba4699c846296a75cf3eb
Gathu17 <jjgathu17@gmail.com>
2024-08-19 16:36:18 +0300

    modify subscriptions table

---
+++ database/migrations/2024_08_19_145625_add_transaction_id_column_in_subscriptions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->foreignId('transaction_id')->nullable()->constrained()->onDelete('cascade');
+            $table->boolean('profile_assigned')->default(false);
+            $table->datetime('expires_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropForeign(['transaction_id']);
+            $table->dropColumn('transaction_id');
+            $table->dropColumn('profile_assigned');
+            $table->dropColumn('expires_at');
+        });
+    }
+};
