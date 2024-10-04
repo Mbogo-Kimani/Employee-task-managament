525213f2197a56dcc0d2c8129ccc54725db7103c
mwitimm <mwendamarto7@gmail.com>
2024-10-04 16:35:24 +0300

    removed the slider on home page added responsiveness

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,175 +90,6 @@
                         <a href="#" class="see-video"></a>
                     </div>
                 </li>
-                <li data-transition="random">
-
-                    <!-- Main Image -->
-                    
-                    <img src="images/rev-slider/4.jpg" alt="" data-bgposition="center center" data-no-retina>
-                    <div class="overlay"></div>
-
-                    <!-- Layers -->
-                    
-                    <div class="tp-caption tp-resizeme text-white text-one"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['-162','-175','-160','-190']"
-                         data-fontsize="['55','55','35','26']"
-                         data-lineheight="['82','82','50','38']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="1000"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on"></div>
-
-                    <div class="tp-caption tp-resizeme text-white text-two"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['-38','-40','-60','-80']"
-                         data-fontsize="['70','70','55','42']"
-                         data-lineheight="['82','82','70','54']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="1600"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on"><br></div>
-
-                    <div class="tp-caption tp-resizeme text-white btn-three"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['83','80','40','40']"
-                         data-fontsize="['19','19','17','16']"
-                         data-lineheight="['30','30','28','28']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="2200"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on"></div>
-
-                    <div class="tp-caption btn-text btn-linear hv-linear-gradient"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['155','160','120','130']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="2800"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on">
-                        <a href="#" class="all-solution font-style linear-color border-corner"><span
-                                class="icon-arrow-pointing-to-right"></span></a>
-                        <a href="#" class="see-video"></a>
-                    </div>
-                </li>
-                <!-- /End Slide 4 -->
-                <!-- Slide 5 -->
-                <li data-transition="random">
-                    <!-- Main Image -->
-                    <img src="images/rev-slider/5.jpg" alt="" data-bgposition="center center" data-no-retina>
-                    <div class="overlay"></div>
-
-                    <!-- Layers -->
-
-                    <div class="tp-caption tp-resizeme text-white text-one"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['-162','-175','-160','-190']"
-                         data-fontsize="['55','55','35','26']"
-                         data-lineheight="['82','82','50','38']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="1000"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on"></div>
-
-                    <div class="tp-caption tp-resizeme text-white text-two"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['-38','-40','-60','-80']"
-                         data-fontsize="['70','70','55','42']"
-                         data-lineheight="['82','82','70','54']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="1600"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on"><br></div>
-
-                    <div class="tp-caption tp-resizeme text-white btn-three"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['83','80','40','40']"
-                         data-fontsize="['19','19','17','16']"
-                         data-lineheight="['30','30','28','28']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="2200"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on"></div>
-
-                    <div class="tp-caption btn-text btn-linear hv-linear-gradient"
-                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                         data-y="['middle','middle','middle','middle']" data-voffset="['155','160','120','130']"
-                         data-width="full"
-                         data-height="none"
-                         data-whitespace="normal"
-                         data-transform_idle="o:1;"
-                         data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                         data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                         data-mask_in="x:0px;y:[100%];"
-                         data-mask_out="x:inherit;y:inherit;"
-                         data-start="2800"
-                         data-splitin="none"
-                         data-splitout="none"
-                         data-responsive_offset="on">
-                        <a href="#" class="all-solution font-style linear-color border-corner"><span
-                                class="icon-arrow-pointing-to-right"></span></a>
-                        <a href="#" class="see-video"></a>
-                    </div>
-                </li>
-
-                <!-- end of slider5 -->
-
             </ul>
         </div>
     </div>
@@ -534,6 +365,110 @@
     transition: background 0.3s ease;
     text-transform: uppercase;
     font-weight: 700;
+}
+
+/* Default styles for larger screens */
+.flat-slider .rev_slider_wrapper {
+    width: 100%;
+    height: auto;
+}
+
+.flat-slider img {
+    max-width: 100%;
+    height: auto;
+}
+
+.tp-caption {
+    font-size: inherit;
+    line-height: inherit;
+}
+
+/* Media queries for responsiveness */
+@media screen and (max-width: 1200px) {
+    .tp-caption.text-one {
+        font-size: 45px;
+        line-height: 65px;
+    }
+
+    .tp-caption.text-two {
+        font-size: 60px;
+        line-height: 75px;
+    }
+
+    .tp-caption.btn-three {
+        font-size: 18px;
+        line-height: 26px;
+    }
+}
+
+@media screen and (max-width: 992px) {
+    .tp-caption.text-one {
+        font-size: 35px;
+        line-height: 50px;
+    }
+
+    .tp-caption.text-two {
+        font-size: 55px;
+        line-height: 70px;
+    }
+
+    .tp-caption.btn-three {
+        font-size: 17px;
+        line-height: 28px;
+    }
+
+    .flat-slider img {
+        width: 100%;
+        height: auto;
+    }
+}
+
+@media screen and (max-width: 768px) {
+    .tp-caption.text-one {
+        font-size: 26px;
+        line-height: 38px;
+    }
+
+    .tp-caption.text-two {
+        font-size: 42px;
+        line-height: 54px;
+    }
+
+    .tp-caption.btn-three {
+        font-size: 16px;
+        line-height: 24px;
+    }
+
+    .flat-slider img {
+        width: 100%;
+        height: auto;
+    }
+}
+
+@media screen and (max-width: 576px) {
+    .tp-caption.text-one {
+        font-size: 22px;
+        line-height: 32px;
+    }
+
+    .tp-caption.text-two {
+        font-size: 36px;
+        line-height: 48px;
+    }
+
+    .tp-caption.btn-three {
+        font-size: 15px;
+        line-height: 22px;
+    }
+
+    .flat-slider img {
+        width: 100%;
+        height: auto;
+    }
+
+    .flat-slider .rev_slider_wrapper {
+        padding: 0;
+    }
 }
 
 
